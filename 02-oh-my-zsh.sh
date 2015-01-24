a84c18ef7a5737658414acedaa5eed65d555f9c1
lavcraft <lavcraft@gmail.com>
2015-01-24 19:29:26 +0300

    Fix oh-my-zsh custom installer

--- 02-oh-my-zsh.sh
+++ 02-oh-my-zsh.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $BASH_SOURCE)
 
-yaourt -S oh-my-zsh-git
+#yaourt -S oh-my-zsh-git
+curl -L http://install.ohmyz.sh | sh
 
 #Install config
-
-ln -sf `pwd`/configs/home/.oh-my-zsh/custom ~/.oh-my-zsh/cystom
+ln -sf `pwd`/configs/home/.oh-my-zsh/custom/themes ~/.oh-my-zsh/custom/themes
